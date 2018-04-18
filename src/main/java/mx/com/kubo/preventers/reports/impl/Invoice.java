a248fce9426cac7d47c5c5f871b2624a4e4e76d4
diegoazd <diegoazd@hotmail.com>
2018-04-18 13:41:18 -0500

    refactor dependency inverion bill printer

--- src/main/java/mx/com/kubo/preventers/reports/impl/Invoice.java
+++ src/main/java/mx/com/kubo/preventers/reports/impl/Invoice.java
@@ -1,6 +1,10 @@
 package mx.com.kubo.preventers.reports.impl;
 
 import mx.com.kubo.preventers.reports.InvoicePrinter;
+import mx.com.kubo.preventers.reports.print.BillPrinter;
+import mx.com.kubo.preventers.reports.print.impl.AsciiPrinter;
+import mx.com.kubo.preventers.reports.print.impl.HtmlPrinter;
+import mx.com.kubo.preventers.reports.print.impl.XmlPrinter;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,62 +22,29 @@ public class Invoice implements InvoicePrinter {
 
     @Override
     public String asciiStatement() {
-        total = BigDecimal.ZERO;
-        StringBuffer sb = new StringBuffer("Bill for ").append(customer).append("\n");
-        lineItem.stream().forEach(item -> {sb.append("\t")
-                                            .append(item.getName())
-                                            .append("\t\t")
-                                            .append(item.getPrice())
-                                            .append("\n");
-           total = total.add(item.getPrice());
-        });
-
-        sb.append("total owed: ").append(total);
-        return sb.toString();
+        return statement(new AsciiPrinter());
     }
 
     @Override
     public String htmlStatement() {
-        total = BigDecimal.ZERO;
-        StringBuffer sb = new StringBuffer("<p>Bill for <i>").append(customer)
-                .append("</i></p><table>");
-        lineItem.stream().forEach(item -> {
-            sb.append("<tr><td>")
-                    .append(item.getName())
-                    .append("</td>")
-            .append("<td>")
-            .append(item.getPrice())
-            .append("</td></tr>");
-            total = total.add(item.getPrice());
-        });
-        sb.append("</table>")
-                .append("<p>total owed: <b>")
-                .append(total)
-                .append("</b></p>");
-
-        return sb.toString();
+        return statement(new HtmlPrinter());
     }
 
     @Override
     public String xmlStatement() {
-        total = BigDecimal.ZERO;
-        StringBuffer sb = new StringBuffer();
-        sb.append("<bill><to>Bill for ").append(customer).append("</to><items>");
-        lineItem.stream().forEach(item -> {
-            sb.append("<item><name>")
-                    .append(item.getName())
-                    .append("</name>")
-                    .append("<price>")
-                    .append(item.getPrice())
-                    .append("</price>")
-                    .append("</item>");
-            total = total.add(item.getPrice());
-        });
-        sb.append("</items><total>total owed: ")
-                .append(total)
-                .append("</total></bill>");
+        return statement(new XmlPrinter());
+    }
 
-        return sb.toString();
+    private String statement(BillPrinter billPrinter) {
+        String s = billPrinter.header(customer);
+        total = BigDecimal.ZERO;
+        for(Item item: lineItem) {
+            s+=billPrinter.item(item);
+            total = total.add(item.getPrice());
+        }
+
+        s+=billPrinter.total(total);
+        return s;
     }
 
     public List<Item> getLineItem() {
